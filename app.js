4ec73fcedb8dcbd67d7d98da2fdc3c7e27680c85
Bemine5Cent <vathanahas.hsv@gmail.com>
2025-07-03 11:02:15 +0700

    Updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Updated, GitHub Actions!');
+    res.send('Updated GitHub Actions!');
 });
 
 app.listen(port, () => {
